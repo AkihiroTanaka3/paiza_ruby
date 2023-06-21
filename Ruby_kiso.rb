d7adff3b3b8923e39ce6f4325cb0001581b70302
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-21 20:15:48 +0900

    Ruby基礎復習puts

---
+++ Ruby_kiso.rb
@@ -0,0 +1,61 @@
+puts "hello paizaラーニング"
+puts "hello world"
+# Here your code !
+# puts "勇者は、荒野を歩いていた"
+puts "モンスターがあらわれた"
+# Here your code !
+puts "荒野を歩いていた"
+# puts "A:モンスターがあらわれた"
+puts "すごい剣を手にいれた"
+# puts "A:ドラゴンがあらわれた"
+# puts "A:魔王があらわれた"
+puts "世界を救った"
+# Here your code !
+puts "<p>勇者は荒野を歩いていた</p>"
+puts "<b>モンスター</b>があらわれた"
+# Here your code !
+player = "勇者"
+puts "#{player}はレベルアップした"
+puts "#{player}の体力が回復した"
+team = "勇者と戦士"
+puts "#{team}の体力が回復した"
+player = "勇者"
+puts "#{ player }は荒野を歩いていた"
+puts "#{ player }はモンスターと戦った"
+puts "#{ player }はモンスターをたおした"
+number = rand(1..6)
+puts "サイコロの目は#{number}です"
+number = rand(50..99)
+puts "モンスターに、#{number}のダメージを与えた"
+
+number = 1234 * 5678 / 2
+puts number
+
+a = 31
+b = 17
+# 以下に、aとbのかけ算し、
+# 結果を出力するコードを書いてください
+puts a * b
+
+x = 8
+y = 5
+# 以下に、xをyで割ったときの余りを計算し、
+# 結果を出力するコードを書いてください
+puts x % y
+# Here your code !
+number = (1234 + 5678) * 3
+puts number
+# Here your code !
+number = rand(1..10)
+money = 500 * number
+puts "スライムが#{number}匹あらわれた"
+puts "スライムを倒したときの獲得金額は#{money}円です"
+# データ型
+x = 50
+puts x - 10
+a = "モンスターが"
+b = "あらわれた"
+puts a+b
+a = "01234"
+b = "56789"
+puts a+b
