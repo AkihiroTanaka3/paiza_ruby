3b2d8f1bcb535236cb2c7719a9ffa8a58712a3ef
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-22 17:33:32 +0900

    Ruby基礎復習for文

---
+++ Ruby_loop.rb
@@ -0,0 +1,22 @@
+# for inによるループ処理
+
+# for カウンタ変数 in 繰り返す範囲
+#     繰り返し処理
+# end 
+for i in 5..10  #0, 1, 2,  ... 4
+    puts "hello world #{i}" #繰り返し処理
+end
+puts "last #{i}"
+# for in によるループ処理
+for i in 0..4
+    puts "ハロー、paizaラーニング"
+end
+# for in によるループ処理
+for i in 0..15
+    puts i
+end
+# for in によるループ処理
+for i in 1..12
+    puts "#{i}月"
+end
+
