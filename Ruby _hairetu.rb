16f102a645e81eb205a7947559716f9baeac95eb
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-25 23:00:04 +0900

    Ruby基礎復習、配列とループ処理

--- Ruby _hairetu.rb
+++ Ruby _hairetu.rb
@@ -65,3 +65,53 @@ puts team.length
 team.delete_at(2)
 p team
 puts team.length
+
+# 配列に要素を追加する
+weapon = ["木の棒", "鉄の棒", "鉄の剣", "銅の剣"]
+# ここに、要素を追加するコードを記述する
+weapon.push("石斧")
+p weapon
+
+# 配列の要素を上書きする
+weapon = ["木の棒", "鉄の棒", "鉄の剣", "サビた剣"]
+# ここに、要素を上書きするコードを記述する
+weapon[3] = "石斧" 
+p weapon
+
+# 配列の要素を削除する
+weapon = ["木の棒", "鉄の棒", "鉄の剣", "銅の剣"]
+# ここに、要素を削除するコードを記述する
+weapon.delete_at(2)
+p weapon
+
+# 配列の要素の個数を出力する
+weapon = ["木の棒", "鉄の棒", "鉄の剣", "銅の剣", "石斧", "エクスカリバー"]
+# ここに、要素数を出力するコードを記述する
+puts weapon.length
+
+# ループで配列を操作する
+team = ["勇者", "戦士", "魔法使い", "盗賊"]
+p team
+# puts team[0]
+puts "<select name='job'>" 
+for job in team
+    puts "<option>#{job}</option>"
+end
+puts "</option>"
+
+#配列の中身をループで表示する
+
+enemy = ["スライム", "モンスター", "ゾンビ", "ドラゴン", "魔王"]
+# ここに、要素をループで表示するコードを記述する
+for i in enemy
+    puts "#{i}が現れた"
+end
+
+# 要素を合計を計算する
+numbers = [12, 34, 56, 78, 90]
+sum = 0
+for num in numbers
+	# ここに、合計を計算するコードを記述する
+sum += num
+end
+puts sum
