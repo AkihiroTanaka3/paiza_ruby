a4970164e3250c143d20ef49db64391253d2e6e9
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-26 22:11:53 +0900

    Ruby基礎復習、配列を使ったおみくじ

--- Ruby_hairetu.rb
+++ Ruby_hairetu.rb
@@ -185,3 +185,40 @@ while line = gets
     array.push(line)
 end
 p array
+
+# 配列を使ったランダムくじ
+# スライム,モンスター,ドラゴン,魔王
+line = gets.chomp.split(",")
+line.each do |enemy|
+	puts "#{enemy}が、現れた！"
+end
+
+# ランダムな数を作る範囲を調べる
+num = line.length
+puts "敵は#{num}匹"
+# ランダムな数を生成
+attack = rand(num)
+# 選んだ敵に、「会心の一撃！」と表示
+puts "#{line[attack]}に会心の一撃！ #{line[attack]}を倒した！"
+
+# じゃんけんプログラム
+# 標準入力から1行取得
+line = gets.chomp
+# カンマで分割して、配列に代入
+janken = line.split(",")
+# 配列の要素数を変数に代入
+num = janken.length
+# 配列の中身を出力
+p janken
+hand = rand(num)
+# ランダムに選んだ配列の要素を出力
+puts janken[hand]
+
+# おみくじプログラム
+line = gets.chomp
+# 今回は自力で全部書いてみよう！
+omikuji = line.split(",")
+num = omikuji.length
+p omikuji
+kekka = rand(num)
+puts omikuji[kekka]
