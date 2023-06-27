77dc8e604199f715804c0854b848789d9a55ee69
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-06-27 17:04:27 +0900

    Ruby基礎復習ハッシュ

--- Ruby_hash.rb
+++ Ruby_hash.rb
@@ -23,3 +23,40 @@ p skills
 skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
 # この下で、ハッシュから出力してみよう
 puts skills["職業"]
+
+# ハッシュの基本操作
+# ハッシュのおさらい
+enemies = {"ザコ" => "スライム", "中ボス" => "ドラゴン", "ラスボス" => "魔王"}
+p enemies
+puts enemies["中ボス"]
+puts enemies.length
+
+enemies["ザコ2"] = "メタルモンスター"
+p enemies
+puts enemies.length
+
+enemies["中ボス"] = "レッドドラゴン"
+p enemies
+puts enemies.length
+
+enemies.delete("ザコ")
+p enemies
+puts enemies.length
+
+# ハッシュの長さを出力する
+skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
+# この下で、ハッシュの長さを出力してみよう
+puts skills.length
+
+# ハッシュに要素を追加する
+skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
+# この下で、ハッシュにデータを追加してみよう
+skills["属性"] = "炎"
+p skills
+
+# ハッシュの要素を削除する
+skills = {"職業" => "戦士", "体力" => 100, "魔法力" => 200, "ゴールド" => 380}
+p skills
+# この下で、ハッシュを削除してみよう
+skills.delete("体力")
+p skills
