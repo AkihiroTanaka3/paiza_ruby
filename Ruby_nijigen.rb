6161890357685f3bfbb86de87e104bfa238be96b
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-03 22:12:53 +0900

    Ruby基礎復習、each_with_index

--- Ruby_nijigen.rb
+++ Ruby_nijigen.rb
@@ -292,3 +292,41 @@ text.each_with_index do |line, i|
     print "#{i + 1}:"
 	puts line
 end
+
+# 2次元配列で地図を表示する
+landmap = Array.new(10).map{Array.new(20,"森")}
+landmap[0][0] = "城"
+landmap[0][19] = "町"
+landmap[9][19] = "町"
+landmap.each_with_index do |line, i|
+    print "#{i}:"
+    line.each_with_index do |area, j|
+        if (i % 2 == 0 || j % 3 == 0) && area == "森"
+            print "＋"
+        else
+            print area
+        end
+    end
+    puts ""
+end
+
+# 初期の地図
+landmap = Array.new(10).map{Array.new(19,"森")}
+landmap[2][9] = "城"
+landmap[0][0] = "町"
+landmap[0][18] = "町"
+landmap[9][0] = "町"
+landmap[9][18] = "町"
+
+# 地図に道を作る
+landmap.each_with_index do |line, i|
+    line.each_with_index do |area, j|
+        # ここから足りないところを入力してください
+        if (i % 9 == 0 || j % 9 == 0) && area == "森"
+            print "＋"
+        else
+            print area
+        end
+    end
+    puts ""
+end
