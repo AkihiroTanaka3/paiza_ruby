6ba96c9d2e0d67eaa3fa111fe487d9bae14068d9
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-05 13:48:24 +0900

    Ruby基礎復習、引数と戻り値

--- Ruby_method.rb
+++ Ruby_method.rb
@@ -27,3 +27,48 @@ def say_hello()
     puts "hello paiza"
 end
 say_hello()
+
+# 引数と戻り値を追加する
+
+def sum(x, y)
+    x + y
+    # return 100
+end
+num1 = sum(3, 4)
+puts num1
+num2 = sum(300, 400)
+puts num2
+
+# 掛け算メソッドを呼び出してみよう
+
+def multi(x, y)
+    return x * y
+end
+# この下にメソッド呼び出しを記述する
+num = multi(2, 6)
+puts num
+
+# 掛け算メソッドを作成してみよう
+
+def multi(x, y)
+    # この下に処理を記述する
+    return x * y
+end
+puts multi(3, 4)
+puts multi(5, 7)
+puts multi(12, 34)
+
+# 九九の表を作成してみよう
+def multi(x, y)
+    return x * y
+end
+
+for i in 1..9
+    for num in 1..9
+    print multi(i, num)
+    if num < 9
+        print ", "
+    end
+    end
+    puts
+end
