f11981ba63761d5973d18ef77af6430272434fad
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-08 13:59:13 +0900

    Ruby基礎復習、インスタンス変数

--- Ruby_class.rb
+++ Ruby_class.rb
@@ -48,3 +48,66 @@ end
 # paiza = greeting.new()
 paiza = Greeting.new()
 paiza.say_hello()
+
+# クラスで変数を管理する
+class Player
+    attr_accessor :job
+    def initialize(job)
+        @job = job
+    end
+    
+    def walk()
+        puts "#{@job}は荒野を歩いていた。"
+    end
+end
+
+player1 = Player.new("戦士")
+player1.walk()
+
+player2 = Player.new("魔法使い")
+player2.walk()
+player1.walk()
+
+player1.job = "勇者"
+player1.walk()
+
+# インスタンスを実体化しよう
+class Greeting
+    def initialize(name)
+        @name = name
+    end
+
+    def say_hello()
+        puts "hello #{@name}"
+    end
+end
+# この下に、インスタンスを実体化し、メソッド呼び出しを記述する
+hello1 = Greeting.new("paiza")
+hello1.say_hello
+
+# クラスにインスタンス変数を追加しよう
+class Greeting
+    def initialize(name)
+        # この下に、initializeメソッドの処理を記述する
+    @name = name
+    end
+
+    def say_hello()
+        puts "hello #{@name}"
+    end
+end
+ruby = Greeting.new("paiza")
+
+# 間違い探し
+class Greeting
+    def initialize(name)
+        @name = name
+    end
+
+    def say_hello()
+        # puts "hello #{name}" @を付ける
+        puts "hello #{@name}"
+    end
+end
+ruby = Greeting.new("paiza")
+ruby.say_hello()
