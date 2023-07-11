17083bf3a0e17a099554a8c27058ce41d49ef45a
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-11 19:58:38 +0900

    Ruby基礎復習、モジュール

--- Ruby_class2.rb
+++ Ruby_class2.rb
@@ -432,3 +432,72 @@ puts time
 require "date"
 date = Date.parse("17.06.30")
 puts date
+
+# モジュールを使う
+module Greeting
+    def say_hello()
+        puts "hello world"
+    end
+    
+    def say_morning()
+        puts "good morning! world"
+    end
+    
+    module_function :say_hello
+end
+
+class Player
+    include Greeting
+    
+    def introduce()
+        puts "私は勇者です。"
+    end
+end
+
+Greeting.say_hello()
+
+player = Player.new()
+player.introduce()
+player.say_morning()
+# player.say_hello() モジュールではできない
+
+# モジュールのメソッドを呼び出す
+module Greeting
+    def say_paiza()
+    	puts "hello paiza"
+    end
+
+    module_function :say_paiza
+end
+
+# この下で、メソッドを呼び出す
+Greeting.say_paiza()
+
+# モジュールのメソッドを呼び出す
+module Greeting
+    def say_paiza()
+    	puts "hello paiza"
+    end
+# ↓追記
+    module_function :say_paiza
+end
+
+Greeting.say_paiza()
+
+# モジュールをクラスに組み込む
+module Action
+    def attack(target)
+    	puts "#{@name}は、#{target}を攻撃した！"
+    end
+end
+
+class Enemy
+    # この下にモジュールを組み込む
+    include Action #追記
+    def initialize(name)
+        @name = name
+    end
+end
+
+slime = Enemy.new("スライム")
+slime.attack("勇者")
