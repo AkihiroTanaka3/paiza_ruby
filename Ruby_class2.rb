6e1ace594c01c588dc2be1290fad7d143372537a
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-11 17:23:58 +0900

    Ruby基礎復習、メソッドのオーバーライド

--- Ruby_class2.rb
+++ Ruby_class2.rb
@@ -175,3 +175,164 @@ end
 player = Hello.new()
 # player.say_hello()
 player.say_hello("ruby")
+
+# RPGのPlayerクラスを継承で記述する
+class Player
+    def initialize(name)
+        @name = name
+    end
+    
+    def attack(enemy)
+        puts "#{@name}は、#{enemy}を攻撃した！"
+    end
+end
+
+puts "=== パーティーでスライムと戦う ==="
+hero = Player.new("勇者")
+warrior = Player.new("戦士")
+party = [hero, warrior]
+
+# p party
+
+party.each do |player|
+    player.attack("スライム")
+end
+
+# RPGのPlayerクラスを継承で記述する その2
+class Player
+    def initialize(name)
+        @name = name
+    end
+
+    def attack(enemy)
+        puts "シャラララーン！"
+        puts "#{@name}は、#{enemy}を攻撃した！"
+    end
+end
+
+class Wizard < Player
+    def attack(enemy)
+        puts "#{@name}は、#{enemy}に炎を放った！"
+    end
+end
+
+puts "=== パーティでスライムと戦う ==="
+hero   = Player.new("勇者")
+warrior = Player.new("戦士")
+wizard = Wizard.new("魔法使い")
+party = [hero, warrior, wizard]
+
+# p party
+
+party.each do |player|
+    player.attack("スライム")
+end
+
+# RPGの攻撃シーン
+class Player
+    def initialize(name)
+        @name = name
+    end
+
+    def attack(enemy)
+        puts  "#{@name}は#{enemy}を攻撃した"
+    end
+end
+
+class Warrior < Player
+    def attack(enemy)
+        puts  "#{@name}は#{enemy}を猛攻撃した！"
+    end
+end
+
+
+team = []
+team.push(Player.new("勇者"))
+team.push(Player.new("魔法使い"))
+team.push(Warrior.new("戦士"))
+
+team.each do |person|
+    person.attack("スライム")
+end
+
+# クラスからメソッドを呼び出す
+class Player
+    def initialize(name)
+        @name = name
+    end
+
+    def attack(enemy)
+        puts "#{@name}は、#{enemy}を攻撃した！"
+    end
+end
+
+class Wizard < Player
+    def initialize()
+        super"魔法使い" #スーパーメソッドを使う
+    end
+    def attack(enemy)
+        spell()
+        puts "#{@name}は、#{enemy}に炎を放った！"
+    end
+    
+    private
+    def spell()
+        puts "シャラララーン！"
+    end
+end
+
+
+puts "=== パーティでスライムと戦う ==="
+hero   = Player.new("勇者")
+warrior = Player.new("戦士")
+wizard = Wizard.new()
+party = [hero, warrior, wizard]
+
+party.each do |player|
+    player.attack("スライム")
+end
+
+# wizard.spell()
+
+# クラスの中のメソッドを呼び出す
+class Greeting
+    def initialize()
+        @msg = "hello"
+        @target = "paiza"
+    end
+    def say_hello()
+        puts "#{@msg} #{@target}"
+        say_yeah()
+    end
+
+    private
+    def say_yeah()
+        puts "YEAH YEAH YEAH!"
+    end
+end
+
+player = Greeting.new()
+player.say_hello()
+
+# 親クラスのメソッドを呼び出す
+class Greeting
+    def initialize()
+        @msg = "hello"
+        @target = "paiza"
+    end
+
+    def say_hello()
+        puts "#{@msg} #{@target}"
+    end
+end
+
+class Hello < Greeting
+    def say_hello()
+        puts "YEAH YEAH YEAH!"
+    end
+end
+
+player = Greeting.new()
+player.say_hello()
+player = Hello.new()
+player.say_hello()
