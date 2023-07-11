8ba7d04d932dfa6932d8c7af40b5e8f08851c97f
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-11 17:41:56 +0900

    Ruby基礎復習、クラス変数とクラスメソッド

--- Ruby_class2.rb
+++ Ruby_class2.rb
@@ -336,3 +336,71 @@ player = Greeting.new()
 player.say_hello()
 player = Hello.new()
 player.say_hello()
+
+# クラス変数とクラスメソッド
+class Player
+    @@charactor_count = 0
+    def self.charactor_count()
+        @@charactor_count
+    end
+    def initialize(name)
+        @name = name
+        @@charactor_count += 1
+        puts "#{@@charactor_count}番目のプレイヤー、#{name}が登場した。"
+    end
+
+    def attack(enemy)
+        puts "#{@name}は、#{enemy}を攻撃した！"
+    end
+end
+
+class Wizard < Player
+    def initialize()
+        super("魔法使い")
+    end
+
+    def attack(enemy)
+        spell()
+        puts "#{@name}は、#{enemy}に炎を放った！"
+    end
+
+    private
+
+    def spell()
+        puts "シャラララーン"
+    end
+end
+
+
+puts "=== パーティでスライムと戦う ==="
+hero   = Player.new("勇者")
+warrior = Player.new("戦士")
+wizard = Wizard.new()
+party = [hero, warrior, wizard]
+
+party.each do |player|
+    player.attack("スライム")
+end
+
+puts "#{Player.charactor_count()}人で、スライムを攻撃した。"
+
+# クラスメソッドを呼び出そう
+class Greeting
+    def self.say_hello()
+        puts "hello paiza"
+    end
+end
+
+# この下で、クラスメソッドを呼び出す
+Greeting.say_hello()
+
+# 間違い探し
+
+class Greeting
+    # def say_hello() self.が必要
+    def self.say_hello()
+        puts "hello paiza"
+    end
+end
+
+Greeting.say_hello()
