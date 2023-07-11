31a09b0dfee4e27cd319d289edcf929e024383ec
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-11 19:43:37 +0900

    Ruby基礎復習、require

--- Ruby_class2.rb
+++ Ruby_class2.rb
@@ -404,3 +404,31 @@ class Greeting
 end
 
 Greeting.say_hello()
+
+# Here your code !
+today = Time.now()
+puts today
+puts today.year
+puts today.month
+puts today.day
+
+require "time"
+time = Time.parse("2023.07.11") do |year|
+    if year < 100
+        if year >= 69
+            year + 1900
+        else
+            year + 2000
+        end
+    else
+        year
+    end
+end
+puts time
+
+# dateライブラリを読み込む
+## この下で、標準添付ライブラリを読み込む
+
+require "date"
+date = Date.parse("17.06.30")
+puts date
