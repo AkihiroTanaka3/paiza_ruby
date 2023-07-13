e95cfcbf05db789525e501cf9b1a0df6853147e0
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-13 23:01:38 +0900

    Ruby基礎復習、発生させる例外を変更

--- Ruby_Exception.rb
+++ Ruby_Exception.rb
@@ -57,3 +57,29 @@ rescue ZeroDivisionError => e
 ensure
     puts "勇者は勝利した"
 end
+
+# 発生させる例外を変えてみよう
+puts 1
+begin
+    number = 1
+    answer = 100 / number
+    puts answer2
+rescue NameError => e
+    puts "未定義の変数を呼び出しています"
+    p e
+ensure
+    puts 2
+end
+
+# 例外の種類を変更しよう
+begin
+    enemies = ["スライム","ドラゴン","魔王"]
+    number1 = 0
+
+    puts "勇者は敵に遭遇した"
+    puts "勇者は#{enemies[number2]}と戦った"
+rescue NameError => e
+    STDERR.puts "未定義の変数を呼び出しています"
+ensure
+    puts "勇者は勝利した"
+end
