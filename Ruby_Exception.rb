04099169d46681e79e2497d2c7532a9ac638de58
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-15 01:28:34 +0900

    Ruby基礎復習、raise

--- Ruby_Exception.rb
+++ Ruby_Exception.rb
@@ -119,3 +119,35 @@ rescue ZeroDivisionError => e
 ensure
     puts "勇者は勝利した"
 end
+
+# raiseで意図的に例外を投げよう
+# raiseを使うと、意図的に例外処理を起動できます。
+# また、rescueと組み合わせることで、メソッドの呼び出し元にある例外処理を利用できます。
+puts 1
+begin
+    puts 2
+    raise Exception.new("意図的な例外")
+    puts 3
+    # number = 1
+    # answer = 100 / number
+    # puts answer
+rescue Exception => e
+    puts "予期せぬエラーが発生しました"
+    p e
+    puts e.message
+ensure
+    # puts 2
+    puts 4
+end
+
+# 例外処理 - 間違い探し
+puts "Hello World"
+
+begin
+    ZeroDivisionError.new
+    STDERR.puts "0では割り算できません"
+rescue NameError
+    STDERR.puts "未定義の変数を呼び出しています"
+ensure
+   puts "Hello Ruby"
+end
