e7dfc043b31546db0f222594f1bdd136fb502bdd
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-07-14 15:54:43 +0900

    Ruby基礎復習、複数の例外を捕捉

--- Ruby_Exception.rb
+++ Ruby_Exception.rb
@@ -83,3 +83,39 @@ rescue NameError => e
 ensure
     puts "勇者は勝利した"
 end
+
+# 複数の例外を捕捉してみよう
+# Exceptionはスーパークラスなので全ての親クラス
+# rescueブロックは、サブクラスから記述する
+puts 1
+begin
+    number = "a"
+    answer = 100 / number
+    puts answer2
+rescue ZeroDivisionError => e
+    puts "0では割り算できません"
+    p e
+rescue NameError => e
+    puts "未定義の変数を呼び出しています"
+    p e
+rescue Exception => e
+    puts "予期せぬエラーが発生しました"
+    p e
+ensure
+    puts 2
+end
+
+# 例外メッセージを指定しよう
+begin
+    enemies = ["スライム","ドラゴン","魔王"]
+    number1 = 0
+
+    puts "勇者は敵に遭遇した"
+    puts "勇者は#{enemies[number2]}と戦った"
+rescue NameError => e
+    STDERR.puts "未定義の変数を呼び出しています"
+rescue ZeroDivisionError => e
+    STDERR.puts "その敵は表示できません"
+ensure
+    puts "勇者は勝利した"
+end
